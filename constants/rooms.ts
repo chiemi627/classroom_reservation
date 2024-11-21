f796043a8a2ee33867d30a36875bab0efce3570e
Chiemi Watanabe <chiemi627@users.noreply.github.com>
2024-11-21 03:50:08 +0000

    部屋の追加（617）

--- constants/rooms.ts
+++ constants/rooms.ts
@@ -2,6 +2,7 @@
 export const ROOMS = [
   { id: '614', label: '614' },
   { id: '615', label: '615' },
+  { id: '617', label: '617' },
   { id: '507', label: '507' },
   { id: '509', label: '509' },
   { id: '520', label: '520' },
